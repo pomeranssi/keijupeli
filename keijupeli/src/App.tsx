cd4ecae0abd81cfb39b3922e43900554cdd64cbf
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-06-14 21:50:24 +0300

    Cleaned up code

--- keijupeli/src/App.tsx
+++ keijupeli/src/App.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import './App.css'
 
-const logo = require('./logo.svg');
+const logo = require('./logo.svg')
 
 class App extends React.Component<{}, null> {
   render() {
@@ -9,14 +9,14 @@ class App extends React.Component<{}, null> {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Keijupeli</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
