a05ee9ced594da08d2cb52856947ce45d3491985
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-06-15 21:08:18 +0300

    Reindented files to 4 spaces

--- keijupeli/src/layout/App.tsx
+++ keijupeli/src/layout/App.tsx
@@ -4,21 +4,21 @@ import './App.css'
 const logo = require('./images/fairy.svg')
 
 class App extends React.Component<{}, null> {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <div className="Logo">
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
-          <h1>Keijupeli</h1>
-        </div>
-        <p className="App-main">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <div className="Logo">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </div>
+                    <h1>Keijupeli</h1>
+                </div>
+                <p className="App-main">
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </div>
+        )
+    }
 }
 
 export default App
