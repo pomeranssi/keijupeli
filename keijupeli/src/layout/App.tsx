e333a513aed6e76e50a68667798b3b4aab43a1e3
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-06-15 08:36:25 +0300

    Reordered files. Added fairy logo

--- keijupeli/src/layout/App.tsx
+++ keijupeli/src/layout/App.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
 import './App.css'
 
-const logo = require('./logo.svg')
+const logo = require('./images/fairy.svg')
 
 class App extends React.Component<{}, null> {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <div className="Logo">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
           <h2>Keijupeli</h2>
         </div>
         <p className="App-intro">
