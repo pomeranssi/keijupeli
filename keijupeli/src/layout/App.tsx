a80b200ccbc0f0d4081970e33d41942fbf44f0d9
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-06-20 18:47:35 +0300

    Fixed toolbar scrolling

--- keijupeli/src/layout/App.tsx
+++ keijupeli/src/layout/App.tsx
@@ -9,9 +9,10 @@ export default class App extends React.Component<{}, null> {
     render() {
         return (
             <div className="App">
-                <div className="App-toolbar">
-                    <ToolBar getGameControl={() => this.gameControl} />
-                    <br />
+                <div className="App-toolbar-container">
+                    <div className="App-toolbar">
+                        <ToolBar getGameControl={() => this.gameControl} />
+                    </div>
                 </div>
                 <div className="App-main">
                     <GameArea ref={r => this.gameControl = r}/>
