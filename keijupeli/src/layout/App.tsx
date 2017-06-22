acdd3165d416cc3294334020c3a03d5318eae222
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-06-22 08:05:17 +0300

    Removed ToolBar component (merged into App)

--- keijupeli/src/layout/App.tsx
+++ keijupeli/src/layout/App.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react'
-import ToolBar from './ToolBar'
 import GameArea from './GameArea'
 import {GameControl} from '../game/GameControl'
 import ItemList from '../game/ItemList'
 import {Category} from '../game/Items'
+const logo = require('./images/fairy.svg')
 import './App.css'
+import CategoryList from '../game/CategoryList'
 
 export default class App extends React.Component<{}, {
     category?: Category
 }> {
     gameControl: GameControl
-    toolBar: ToolBar
+    categoryList: CategoryList
+
     componentWillMount() {
         this.setState({category: undefined})
     }
@@ -18,12 +20,18 @@ export default class App extends React.Component<{}, {
         return (
             <div className="App">
                 <div className="App-main">
-                    <GameArea ref={r => this.gameControl = r} getEventListener={() => this.toolBar.getEventListener()}/>
+                    <GameArea ref={r => this.gameControl = r} getEventListener={() => this.categoryList}/>
                     <br />
                 </div>
                 <div className="App-toolbar primary">
-                    <ToolBar getGameControl={() => this.gameControl} ref={r => this.toolBar = r}
-                             setCategory={c => this.setState({category: c})}/>
+                    <div className="LogoArea">
+                        <div className="Logo">
+                            <img src={logo} className="App-logo" alt="logo" />
+                        </div>
+                        <h1>Keijupeli</h1>
+                    </div>
+                    <CategoryList getGameControl={() => this.gameControl} ref={r => this.categoryList = r}
+                                  setCategory={c => this.setState({category: c})} />
                 </div>
                 <div className="App-toolbar secondary">
                     <ItemList getGameControl={() => this.gameControl} category={this.state.category}/>
