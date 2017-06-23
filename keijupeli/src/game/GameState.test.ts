f7bfbb987a7598a973005692b726e39aa60cbe6f
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-06-23 21:42:49 +0300

    Added tests that check that state is altered correctly

--- keijupeli/src/game/GameState.test.ts
+++ keijupeli/src/game/GameState.test.ts
@@ -1,11 +1,19 @@
 import {addItem, removeItem, state} from './GameState'
 import items from './Items'
 
+function ifd<S, T>(o: S | undefined | null, f: (s: S) => T): T | undefined {
+    return o ? f(o) : undefined
+}
+
 it('adds and removes items from state correctly', () => {
-    state.subscribe(() => console.log(state.getState()))
     const c1 = items[0]
     const c2 = items[1]
     state.dispatch(addItem(c1.items[0], c1))
-    state.dispatch(addItem(c2.items[0], c2))
+    expect(ifd(state.getState(), s => s[c1.type])).toEqual(c1.items[0])
+    state.dispatch(addItem(c2.items[1], c2))
+    expect(ifd(state.getState(), s => s[c1.type])).toEqual(c1.items[0])
+    expect(ifd(state.getState(), s => s[c2.type])).toEqual(c2.items[1])
     state.dispatch(removeItem(c1))
+    expect(ifd(state.getState(), s => s[c2.type])).toEqual(c2.items[1])
+    expect(ifd(state.getState(), s => s[c1.type])).toBeUndefined()
 })
