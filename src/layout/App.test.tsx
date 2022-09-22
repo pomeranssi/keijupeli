8ca30ea39867b22e0df524bae8ca09620a64f7e9
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2022-09-22 08:31:25 +0300

    Fix tests

--- src/layout/App.test.tsx
+++ src/layout/App.test.tsx
@@ -1,9 +1,14 @@
+import { render } from '@testing-library/react';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import { store } from '../game/GameState';
 import { App } from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
