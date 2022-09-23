4aaad5973c5eb1c554df1ec5276c8f3d27245191
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2022-09-23 17:12:08 +0300

    Add DB initialization code

---
+++ src/server/data/Db.ts
@@ -0,0 +1,15 @@
+import debug from 'debug';
+import * as pgp from 'pg-promise';
+
+import { config } from '../config';
+const log = debug('bookkeeper:sql');
+
+const logSql = process.env.LOG_SQL === 'true';
+
+if (logSql) {
+  log('Logging all SQL queries');
+}
+
+export const db = pgp({
+  query: logSql ? q => log(`SQL: ${q.query}`) : undefined,
+})(config.dbUrl);
