02eafa09f942775559219bee22bc3ef01ba0b411
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2022-09-28 21:10:42 +0300

    Remove console logging

--- src/client/game/dataInit.tsx
+++ src/client/game/dataInit.tsx
@@ -6,7 +6,6 @@ import { useGameState } from './state';
 export async function initializeCategories() {
   const sessionId = useGameState.getState().session?.id;
   if (!sessionId) return;
-  console.log('Initializing categories with', sessionId);
   const categories = await getCategories(sessionId);
   useGameState.getState().setupCategories(categories);
 }
