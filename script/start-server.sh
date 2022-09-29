c85401a1b1a7095400adcfc12c722c66fc5e34a5
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2022-09-29 18:48:23 +0300

    Fix server startup cmd

--- script/start-server.sh
+++ script/start-server.sh
@@ -6,6 +6,6 @@ cd `dirname $0`/..
 mkdir -p log
 
 echo "Starting server"
-NODE_ENV=production nohup node -r ./tsconfig-paths-bootstrap.js build-server/server/keijupeliServer.js >log/server.log 2>&1 &
+NODE_ENV=production nohup node -r dotenv/config -r tsconfig-paths/register build-server/server/keijupeliServer.js >log/server.log 2>&1 &
 
 popd >/dev/null
